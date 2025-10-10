436d6fccbd9165547b0b9595f5ffe8c7c1610d2c
Anna Llorens <annallorens_12@hotmail.com>
2025-10-10 16:20:13 +0200

    feat: POC add new plugin

--- dev/builder/build-config.js
+++ dev/builder/build-config.js
@@ -108,6 +108,7 @@ var CKBUILDER_CONFIG = {
         taoqtimedia: 1,
         taoqtitable: 1,
         taounderline: 1,
+        taostrike: 1,
         taotooltip: 1,
         taohighlight: 1,
         taoqtiinclude: 1,
---
+++ plugins/taostrike/lang/en.js
@@ -0,0 +1,4 @@
+CKEDITOR.plugins.setLang("spanStrike", "en", {
+	button: "Strike",
+	title: "Strike",
+});
