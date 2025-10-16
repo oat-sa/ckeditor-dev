9a258eaa69eff1b4b253d61799784c2a724b249f
Anna Llorens <annallorens_12@hotmail.com>
2025-10-16 10:14:40 +0200

    feat: Added new text-decorations

--- plugins/taostrike/plugin.js
+++ plugins/taostrike/plugin.js
@@ -1,41 +1,43 @@
+
 CKEDITOR.plugins.add("taostrike", {
-	lang: "en", // %REMOVE_LINE_CORE%
-	init: function (editor) {
-		var commandName = "spanStrike",
-			style = new CKEDITOR.style({
-				element: "span",
-				attributes: { class: "txt-strike" },
-			}),
-			forms = [
-				"u",
-				[
-					"span",
-					function (el) {
-						return el.styles["text-decoration"] == "strike";
-					},
-				],
-			];
+  lang: "en",
+  icons: "strike",
+  hidpi: true,
 
-		// Put the style as the most important form.
-		forms.unshift(style);
+  init: function (editor) {
+    var commandName = "spanStrike",
+      style = new CKEDITOR.style({
+        element: "span",
+        attributes: { class: "txt-strike" },
+      }),
+      forms = [
+        "strike",
+        [
+          "span",
+          function (el) {
+            return el.styles["text-decoration"] === 'line-through';
+          },
+        ],
+      ];
+// Put the style as the most important form.
+    forms.unshift(style);
 
-		// Listen to contextual style activation.
-		editor.attachStyleStateChange(style, function (state) {
-			!editor.readOnly && editor.getCommand(commandName).setState(state);
-		});
+    editor.attachStyleStateChange(style, function (state) {
+        !editor.readOnly && editor.getCommand(commandName).setState(state);
+    });
 
-		// Create the command that can be used to apply the style.
-		editor.addCommand(
-			commandName,
-			new CKEDITOR.styleCommand(style, {
-				contentForms: forms,
-			})
-		);
+    editor.addCommand(
+      commandName,
+      new CKEDITOR.styleCommand(style, {
+        contentForms: forms,
+      })
+    );
 
-		editor.ui.addButton("TaoStrike", {
-			label: editor.lang[commandName].button,
-			command: commandName,
-			icon: this.path + "images/taostrike.png",
-		});
-	},
+    editor.ui.addButton("TaoStrike", {
+      label: editor.lang[commandName].button,
+      command: commandName,
+      icon: "strike",
+      toolbar: "basicstyles,21",
+    });
+  },
 });
