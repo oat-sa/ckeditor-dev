5d0de57e4bdc496cde2df46ba67ebce4ac164b9a
Anna Llorens <annallorens_12@hotmail.com>
2025-10-21 17:46:51 +0200

    chore: Update version

--- dev/builder/build.sh
+++ dev/builder/build.sh
@@ -57,7 +57,7 @@ echo "Starting CKBuilder..."
 
 JAVA_ARGS=${ARGS// -t / } # Remove -t from args.
 
-VERSION="4.14.1 TAO-3.1"
+VERSION="4.14.1 TAO-3.3"
 REVISION=$(git rev-parse --verify --short HEAD)
 
 # If the current revision is not tagged with any CKE version, it means it's a "dirty" build. We
